dfd3748f7a659d680b95826bd7b95044917c6d9f
Unknown <terry.gonthier@ynov.com>
2018-02-06 12:40:46 +0100

    Auto stash before merge of "master" and "origin/master"
    Categories

---
+++ ProjetInterface/src/beans/Categories.java
@@ -0,0 +1,12 @@
+package beans;
+
+import java.util.ArrayList;
+
+@SuppressWarnings("serial")
+public class Categories extends ArrayList<Categorie>{
+
+	public Categorie getCategorie(int code) {
+		
+		return null;
+	}
+}

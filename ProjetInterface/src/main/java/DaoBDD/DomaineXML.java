cf5f3a8139f400ad6636b4de7d9786fc758e17de
Antoine Poussard <antoinepoussard@pc26.home>
2018-02-18 01:13:06 +0100

    DomaineXML addProduit + addCategorie

--- ProjetInterface/src/main/java/DaoBDD/DomaineXML.java
+++ ProjetInterface/src/main/java/DaoBDD/DomaineXML.java
@@ -1,6 +1,8 @@
 package DaoBDD;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
@@ -16,6 +18,7 @@ import org.jdom2.input.SAXBuilder;
 import IDAO.IDomaine;
 import beans.Categorie;
 import beans.Categories;
+import org.jdom2.output.XMLOutputter;
 
 public class DomaineXML implements IDomaine {
     private String PATH = "utils/";
@@ -27,12 +30,14 @@ public class DomaineXML implements IDomaine {
 		try {
 		    // Get all Categories & Products from XML file
 			File catFile = new File(PATH + "Categorie.xml");
+            FileReader catReader = new FileReader(catFile);
             File prodFile = new File(PATH + "Produit.xml");
+            FileReader prodReader = new FileReader(prodFile);
 
 			SAXBuilder catBuilder = new SAXBuilder();
             SAXBuilder prodBuilder = new SAXBuilder();
 
-			Document catDoc = catBuilder.build(catFile);
+			Document catDoc = catBuilder.build(catReader);
 			Document prodDoc = prodBuilder.build(prodFile);
 
 			Element catElement = catDoc.getRootElement();
@@ -49,7 +54,7 @@ public class DomaineXML implements IDomaine {
                 returnArray.add(tmpCat);
 			}
 
-            for (int temp = 0; temp < listCategorie.size(); temp++) {
+            for (int temp = 0; temp < listProduit.size(); temp++) {
                 Element cat = listProduit.get(temp);
                 int intProd = Integer.parseInt(cat.getChild("codeProduit").getText());
                 String strProd = cat.getChild("nomProduit").getText();
@@ -59,6 +64,8 @@ public class DomaineXML implements IDomaine {
                 Produit p = new Produit(intProd, strProd, floatPrix, intprodCat);
                 returnArray.get(intprodCat - 1).ajouteProduit(p);
             }
+            catReader.close();
+			prodReader.close();
 		} catch(JDOMException e) {
 			e.printStackTrace();
 		} catch(IOException ioe) {
@@ -72,8 +79,9 @@ public class DomaineXML implements IDomaine {
 	public Categorie getCategorie(int i) {
 	    try {
             File catFile = new File(PATH + "Categorie.xml");
+            FileReader catReader = new FileReader(catFile);
             SAXBuilder catBuilder = new SAXBuilder();
-            Document catDoc = catBuilder.build(catFile);
+            Document catDoc = catBuilder.build(catReader);
             Element catElement = catDoc.getRootElement();
             List<Element> listCategorie = catElement.getChildren();
 
@@ -81,7 +89,7 @@ public class DomaineXML implements IDomaine {
             int intCat = Integer.parseInt(cat.getChild("codeCategorie").getText());
             String strCat = cat.getChild("nomCategorie").getText();
             Categorie tmpCat = new Categorie(intCat, strCat);
-
+            catReader.close();
             return  tmpCat;
         } catch(JDOMException e) {
             e.printStackTrace();
@@ -93,11 +101,89 @@ public class DomaineXML implements IDomaine {
 
 	@Override
 	public boolean addCategorie(String s) {
+        try {
+            XMLOutputter out = new XMLOutputter();
+
+            File catFile = new File(PATH + "Categorie.xml");
+            FileReader catReader = new FileReader(catFile);
+
+            SAXBuilder catBuilder = new SAXBuilder();
+            Document CategorieDoc = catBuilder.build(catReader);
+
+            Element catElement = CategorieDoc.getRootElement();
+            List<Element> listCategorie = catElement.getChildren();
+
+            Element e_categorie = new Element("Categorie");
+            Element e_code = new Element("codeCategorie");
+            Element e_nom = new Element("nomCategorie");
+
+            e_code.setText(String.valueOf(listCategorie.size() + 1));
+            e_nom.setText(s);
+
+            e_categorie.addContent(e_code);
+            e_categorie.addContent(e_nom);
+
+            catElement.addContent(e_categorie);
+
+            catReader.close();
+            FileWriter writer = new FileWriter(catFile);
+
+            out.output(catElement, writer);
+            writer.close();
+            return true;
+
+        } catch(JDOMException e) {
+            e.printStackTrace();
+        } catch(IOException ioe) {
+            ioe.printStackTrace();
+        }
 		return false;
 	}
 
 	@Override
 	public boolean addProduit(String s, float f, int i) {
+        try {
+            XMLOutputter out = new XMLOutputter();
+
+            File prodFile = new File(PATH + "Produit.xml");
+            FileReader prodReader = new FileReader(prodFile);
+
+            SAXBuilder catBuilder = new SAXBuilder();
+            Document ProduitDoc = catBuilder.build(prodReader);
+
+            Element prodElement = ProduitDoc.getRootElement();
+            List<Element> listProduit = prodElement.getChildren();
+
+            Element e_categorie = new Element("Produit");
+            Element e_code = new Element("codeProduit");
+            Element e_nom = new Element("nomProduit");
+            Element e_prix = new Element("prixProduit");
+            Element e_codeCategorie = new Element("codeCategorie");
+
+            e_code.setText(String.valueOf(listProduit.size() + 1));
+            e_nom.setText(s);
+            e_prix.setText(String.valueOf(f));
+            e_codeCategorie.setText(String.valueOf(i));
+
+            e_categorie.addContent(e_code);
+            e_categorie.addContent(e_nom);
+            e_categorie.addContent(e_prix);
+            e_categorie.addContent(e_codeCategorie);
+
+            prodElement.addContent(e_categorie);
+
+            prodReader.close();
+            FileWriter writer = new FileWriter(prodFile);
+
+            out.output(prodElement, writer);
+            writer.close();
+            return true;
+
+        } catch(JDOMException e) {
+            e.printStackTrace();
+        } catch(IOException ioe) {
+            ioe.printStackTrace();
+        }
 		return false;
 	}
 
